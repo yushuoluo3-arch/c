d1cfc780db0b6bf8ebf553ea11050b02197c2bd1
yushuoluo3-arch <yushuoluo3@gmail.com>
2026-09-12 22:17:38 +0900

    更新 main.c

--- day01/main.c
+++ day01/main.c
@@ -4,8 +4,19 @@ int main(void)
 {
     // Day 01: 变量、数据类型与 printf
     // 请自己完成今天的练习。
+//输出自己的年龄。
+//定义一个 double 变量保存自己的身高，并输出。
+//定义一个 char 变量保存一个字母，并输出。
+//定义三个变量：年龄、身高、体重，并一次性输出。
+//修改 main.c，让程序输出三行不同的信息。
 
-    printf("Hello, C!\n");
+    int age = 21;
+    double high = 170;
+    char a = 'a';
+    
+    printf("age\n");
+    printf("high\n");
+    printf("a\n");
 
     return 0;
 }
