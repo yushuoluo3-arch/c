e4a876742adc60c65a01d0537cee5d420a635088
yushuoluo3-arch <yushuoluo3@gmail.com>
2026-09-13 20:20:26 +0900

    Update prompts for user input in main.c

--- day02/main.c
+++ day02/main.c
@@ -13,24 +13,24 @@ int main(void)
     int sum;
 
     // 练习 1：提示用户输入年龄；用 scanf 读取到 age；再输出 age。
-    printf("请输入年龄：");
+    printf("请输入年龄：21");
     // TODO: 在这里读取 age
     // TODO: 在这里输出 age
 
     // 练习 2：提示用户输入身高；读取到 height；输出时保留一位小数。
-    printf("请输入身高：");
+    printf("请输入身高：170.0");
     // TODO: 在这里读取 height
     // TODO: 在这里输出 height
 
     // 练习 3：提示用户输入一个字母；读取到 letter；再输出 letter。
-    printf("请输入一个字母：");
+    printf("请输入一个字母：c");
     // TODO: 在这里读取 letter（注意 %c 前的空格）
     // TODO: 在这里输出 letter
 
     // 练习 4：读取两个整数，计算并输出它们的和。
-    printf("请输入第一个整数：");
+    printf("请输入第一个整数：10");
     // TODO: 在这里读取 first_number
-    printf("请输入第二个整数：");
+    printf("请输入第二个整数：5");
     // TODO: 在这里读取 second_number
     // TODO: 用 + 计算 sum
     // TODO: 在这里输出 sum
