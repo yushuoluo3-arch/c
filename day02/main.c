bffca657d34f8fffe0db78283cd5e7456f32cd86
yushuoluo3-arch <yushuoluo3@gmail.com>
2026-09-13 19:48:58 +0900

    Add Day 02 scanf practice framework
    Added a C program with user input exercises for age, height, letter, and sum of two integers.

---
+++ day02/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Day 02：scanf 输入练习
+    // 请根据 notes.md 自己完成下面的每个 TODO。
+
+    int age;
+    double height;
+    char letter;
+    int first_number;
+    int second_number;
+    int sum;
+
+    // 练习 1：提示用户输入年龄；用 scanf 读取到 age；再输出 age。
+    printf("请输入年龄：");
+    // TODO: 在这里读取 age
+    // TODO: 在这里输出 age
+
+    // 练习 2：提示用户输入身高；读取到 height；输出时保留一位小数。
+    printf("请输入身高：");
+    // TODO: 在这里读取 height
+    // TODO: 在这里输出 height
+
+    // 练习 3：提示用户输入一个字母；读取到 letter；再输出 letter。
+    printf("请输入一个字母：");
+    // TODO: 在这里读取 letter（注意 %c 前的空格）
+    // TODO: 在这里输出 letter
+
+    // 练习 4：读取两个整数，计算并输出它们的和。
+    printf("请输入第一个整数：");
+    // TODO: 在这里读取 first_number
+    printf("请输入第二个整数：");
+    // TODO: 在这里读取 second_number
+    // TODO: 用 + 计算 sum
+    // TODO: 在这里输出 sum
+
+    return 0;
+}
